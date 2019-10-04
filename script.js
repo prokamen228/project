1d31c5f4b3858c74ec461c64c326d9f1a7c4c9ea
alexandr <example@gmail.com>
2019-10-04 18:30:06 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+'use script';
+
+var number = 5;
+var string = "Hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log('string'*9);
+console.log();
+
